09bcda7b78b8cc5c02ecd31823a32730e47988eb
Eduard Kazeks <e.kazeks@gmail.com>
2021-05-05 11:19:13 +0300

    updated part1 task

--- part1/src/App.js
+++ part1/src/App.js
@@ -8,13 +8,10 @@ const Part = (props) => {
 
   const Header = (props) => {
     console.log(props)
-   return <h1>{props.course.name}</h1>
+   return <h1>{props.course.courseName}</h1>
  }
 
  const  content = [
-  {
-    name: 'Half Stack application development'
-  },
   {
     name: 'Fundamentals of React', exercises: 10
   },
@@ -27,25 +24,39 @@ const Part = (props) => {
 ]
  const Total = ({content}) => {
   return (
-    <p>Number of exercises: '{content[1].exercises + content[2].exercises + content[3].exercises}'</p>
+    <p>Number of exercises: {content[0].exercises + content[1].exercises + content[2].exercises}</p>
   );
 };
 
 const App = () => {
 
+  const course = {
+    courseName: 'Half Stack application development',
+    content: [
+      {
+        name: 'Fundamentals of React', exercises: 10
+      },
+      {
+        name: 'Using props to pass data', exercises: 7
+      },
+      {
+        name: 'State of a component', exercises: 14
+      }
+    ]
+  }
   const Content = (props) => {
     return (
       <div>
+        <Part part={props.content[0]} />
         <Part part={props.content[1]} />
         <Part part={props.content[2]} />
-        <Part part={props.content[3]} />
       </div>
     )
   }
 
   return (
     <div>
-      <Header course={content[0]} />
+      <Header course={course} />
       <Content content={content} />
       <Total content={content} />
     </div>
